05c285ffcfda99747de20b7a8ab2d1132e3187fd
Mel <melissabrito2015@outlook.com.br>
2023-06-18 19:14:28 -0300

    at

---
+++ EstruturasdeProgramacao/13_for/script.js
@@ -0,0 +1,7 @@
+for(let numero = 2; numero < 100; numero = numero *2) {
+  console.log("o numero é: " + numero);
+}
+
+for (let i = 0; i < 100; i = i + 3) {
+  console.log(`A soma de i com 2 é: ${i + 2}`);
+} 
